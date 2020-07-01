65e2ea663f25043108dd17bcb2a2f9b28b0af65a
Artyom Davydov <davart01@gmail.com>
2020-07-01 16:25:06 +0400

    added shared_container.h

---
+++ bigint-optimized/shared_container.h
@@ -0,0 +1,13 @@
+//
+// Created by dave11ar on 30.06.2020.
+//
+
+#ifndef BIGINT__SHARED_CONTAINER_H_
+#define BIGINT__SHARED_CONTAINER_H_
+
+#include <vector>
+
+template <typename T>
+
+
+#endif //BIGINT__SHARED_CONTAINER_H_
