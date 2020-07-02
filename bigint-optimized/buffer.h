414703aff51cdc10a86e677a17476238466c5f75
Artyom Davydov <davart01@gmail.com>
2020-07-02 05:34:07 +0400

    Fixed second review

--- bigint-optimized/buffer.h
+++ bigint-optimized/buffer.h
@@ -5,21 +5,12 @@
 #ifndef BIGINT__BUFFER_H_
 #define BIGINT__BUFFER_H_
 
-#include <vector>
-const size_t MAX_SMALL = 2;
-
-template <typename T>
-struct shared_container {
-  size_t ref_counter;
-  std::vector<T> vec;
-
-  ~shared_container()  = default;
-
-  explicit shared_container(std::vector<T> const& a) : ref_counter(1), vec(a) {}
-};
+#include "shared_container.h"
 
 template <typename T>
 struct buffer {
+  static constexpr size_t MAX_SMALL = 2;
+
   bool sign;
 
   buffer(bool sign, T a) : sign(sign), size(1), small(true) {
@@ -33,18 +24,13 @@ struct buffer {
       }
     } else {
       shared_data = a.shared_data;
-      shared_data->ref_counter++;
+      shared_data->increase_ref();
     }
   }
 
   ~buffer() {
-    if (!small) {
-      shared_data->ref_counter--;
-      if (shared_data->ref_counter == 0) {
-        delete shared_data;
-      }
-    } else {
-      destruct_small();
+    if (!small && shared_data->unique()) {
+      delete shared_data;
     }
   }
 
@@ -53,38 +39,51 @@ struct buffer {
       return small_data[i];
     } else {
       unshare();
-      return shared_data->vec[i];
+      return (*shared_data)[i];
     }
   }
 
   T const& operator[](size_t i) const {
-    return small ? small_data[i] : shared_data->vec[i];
+    return small ? small_data[i] : (*shared_data)[i];
   }
 
   T const& back() const {
-    return small ? small_data[size - 1] : shared_data->vec.back();
+    return small ? small_data[size - 1] : shared_data->back();
   }
 
-  size_t const& get_size() const {
+  size_t get_size() const {
     return size;
   }
 
   buffer& operator=(buffer const& a) {
-    this->~buffer();
+    buffer tmp(a);
+    swap(tmp);
+    return *this;
+  }
 
-    sign = a.sign;
-    size = a.size;
-    small = a.small;
-    if (small) {
-      for (size_t i = 0; i < size; i++) {
-        small_data[i] = a.small_data[i];
+  void swap(buffer& a) {
+    using std::swap;
+
+    if (a.small) {
+      if (small) {
+        swap(small_data, a.small_data);
+      } else {
+        shared_container<T>* tmp = shared_data;
+        std::copy(a.small_data, a.small_data + a.size, small_data);
+        a.shared_data = tmp;
       }
     } else {
-      shared_data = a.shared_data;
-      shared_data->ref_counter++;
+      if (small) {
+        shared_container<T>* tmp = a.shared_data;
+        std::copy(small_data, small_data + size, a.small_data);
+        shared_data = tmp;
+      } else {
+        swap(shared_data, a.shared_data);
+      }
     }
-
-    return *this;
+    swap(small, a.small);
+    swap(sign, a.sign);
+    swap(size, a.size);
   }
 
   friend bool operator==(buffer const& a, buffer const& b) {
@@ -105,20 +104,16 @@ struct buffer {
       if (size == MAX_SMALL) {
         small = false;
 
-        std::vector<T> tmp;
-        for (size_t i = 0; i < size; i++) {
-          tmp.push_back(small_data[i]);
-        }
+        std::vector<T> tmp(small_data, small_data + size);
         tmp.push_back(a);
 
-        destruct_small();
         shared_data = new shared_container<T>(tmp);
       } else {
         small_data[size] = a;
       }
     } else {
       unshare();
-      shared_data->vec.push_back(a);
+      shared_data->push_back(a);
     }
     size++;
   }
@@ -128,35 +123,23 @@ struct buffer {
       small_data[size - 1].~T();
     } else {
       unshare();
-      shared_data->vec.pop_back();
+      shared_data->pop_back();
     }
     size--;
   }
 
   void reverse() {
-    size_t mid = size / 2;
     if (small) {
-      for (size_t i = 0; i < mid; i++) {
-        std::swap(small_data[i], small_data[size - 1 - i]);
-      }
+      std::reverse(small_data, small_data + size);
     } else {
       unshare();
-      for (size_t i = 0; i < mid; i++) {
-        std::swap(shared_data->vec[i], shared_data->vec[size - 1 - i]);
-      }
-    }
-  }
-
-  void destruct_small() {
-    for (size_t i = size; i > 0; i--) {
-      small_data[i].~T();
+      shared_data->reverse();
     }
   }
 
   void unshare() {
-    if (shared_data->ref_counter != 1) {
-      shared_data->ref_counter--;
-      shared_data = new shared_container<T>(shared_data->vec);
+    if (!shared_data->unique()) {
+      shared_data = new shared_container<T>(*shared_data);
     }
   }
 
